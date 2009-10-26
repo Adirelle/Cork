0a01648e065ca8fa45d5cd65a9de98f1bf156b96
Adirelle <adirelle@tagada-team.net>
2009-10-26 10:55:03 +0100

    Combine: handle each combining item kind separately.

--- modules/Combine.lua
+++ modules/Combine.lua
@@ -3,7 +3,6 @@ local Cork = Cork
 local IconLine = Cork.IconLine
 local ldb, ae = LibStub:GetLibrary("LibDataBroker-1.1"), LibStub("AceEvent-3.0")
 
-local MODULE = "Combine"
 local ITEMS = {
 	[37700] = 10, -- Crystallized Air
 	[37701] = 10, -- Crystallized Earth
@@ -15,32 +14,33 @@ local ITEMS = {
 	[34056] = 3,  -- Lesser Cosmic Essence
 }
 
-Cork.defaultspc[MODULE.."-enabled"] = true
+Cork.defaultspc["Combine-enabled"] = true
 
-local dataobj = ldb:NewDataObject("Cork "..MODULE, {type = "cork"})
+local dataobj = ldb:NewDataObject("Cork Combine", {type = "cork"})
 
 function dataobj:Scan()
-	if Cork.dbpc[MODULE.."-enabled"] and not InCombatLockdown() then
+	if Cork.dbpc["Combine-enabled"] and not InCombatLockdown() then
 		for id,threshold in pairs(ITEMS) do
 			local count = GetItemCount(id) or 0
 			if count >= threshold then
 				local itemName, _, _, _, _, _, _, _, _, itemTexture = GetItemInfo(id) 
-				dataobj.player = IconLine(itemTexture, itemName.." ("..count..")")
-				return
+				dataobj[id] = IconLine(itemTexture, itemName.." ("..count..")")
 			end
+		end
+	else
+		for id in pairs(ITEMS) do
+			dataobj[id] = nil
 		end
 	end
 end
 
-ae.RegisterEvent("Cork "..MODULE, "BAG_UPDATE", dataobj.Scan)
+ae.RegisterEvent("Cork Combine", "BAG_UPDATE", dataobj.Scan)
 
 function dataobj:CorkIt(frame)
-	if dataobj.player then
-		for id,threshold in pairs(ITEMS) do
-			if (GetItemCount(id) or 0) >= threshold then 
-				dataobj.player = nil
-				return frame:SetManyAttributes("type1", "item", "item1", "item:"..id) 
-			end
+	for id,threshold in pairs(ITEMS) do
+		if dataobj[id] and frame:SetManyAttributes("type1", "item", "item1", "item:"..id) then
+			dataobj[id] = nil -- Do not use it again before bags are updated
+			return true
 		end
 	end
 end
